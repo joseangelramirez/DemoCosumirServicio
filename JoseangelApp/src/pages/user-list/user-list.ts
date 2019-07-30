346d9652ae2f4044e927ca6e910b595f7dc0785d
joseangelramirez <53190814+joseangelramirez@users.noreply.github.com>
2019-07-30 15:56:46 -0400

    Correccion de errores
    Correccion de errores

--- JoseangelApp/src/pages/user-list/user-list.ts
+++ JoseangelApp/src/pages/user-list/user-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
 import { UserinfoPage } from '../userinfo/userinfo';
 
@@ -16,43 +16,40 @@ import { UserinfoPage } from '../userinfo/userinfo';
   templateUrl: 'user-list.html',
 })
 export class UserListPage {
-  // Declaro los objetos de los cuales voy a ahcer uso para 
-  // mostrar la informacion del usuario
-  InfoUser:any ={};
-  PirctureUser:any ={};
-  Cell:any ={};
-  Email:any ={};
-  Phone:any ={};
-  Location:any ={};
-  // Declaro un objeto el cual voy a pasar como parametro al detalle
- ArgsData:any[]=[];
+  Users:any[]=[];
+   
   constructor(public navCtrl: NavController,
-    public navParams: NavParams, private api: ApiProvider) {
+    public navParams: NavParams, private api: ApiProvider, 
+    private loadingCtrl: LoadingController) {
 
-     this.api.getUsers().subscribe((res:any)=>{
-       if(res.results){
-         // Procedo con el llenado de los datos cuando
-         // la peticion http se complete
-        //  console.log(res.results);
-           this.InfoUser=res.results[0].name;
-         this.PirctureUser=res.results[0].picture;
-         this.Cell=res.results[0].cell;
-         this.Email=res.results[0].email;    
-         this.Phone=res.results[0].phone;
-         this.Location=res.results[0].location;
-         // Lleno el arreglo con toda la informacion del Usuario
-         this.ArgsData.push(this.InfoUser,this.PirctureUser,this.Cell,this.Email,this.Phone,this.Location);
-         // console.log(this.ArgsData);
-        }
-       
-         
-     });
+    this.getData();
    
         
   }
-  MostrarInfo(){
-    this.navParams.data=this.ArgsData;
-  this.navCtrl.push(UserinfoPage,this.ArgsData)
+    getData(){
+      const loader = this.loadingCtrl.create({
+        content: "Por favor espere...",
+         
+      });
+      loader.present();
+
+    this.api.getUsers().subscribe((res:any)=>{
+      if(res.results){
+        // Procedo con el llenado de los datos cuando
+        // la peticion http se complete
+       //  console.log(res.results);
+         this.Users=res.results;
+         loader.dismiss();
+        // console.log(this.ArgsData);
+       }
+             
+    },err=>{
+      loader.dismiss();
+    });
+  }
+  MostrarInfo(UserData:any){
+    this.navParams.data=UserData;
+  this.navCtrl.push(UserinfoPage,UserData)
 
   }
 
